a6f611f9835cd44f629feff7934b724b6b874078
Kitemaker <sj112233@gmail.com>
2018-05-01 22:54:07 +0530

    created second project

--- myconsoleapp/Program.cs
+++ myconsoleapp/Program.cs
@@ -6,7 +6,7 @@ namespace myconsoleapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Saleem!");
         }
     }
 }
